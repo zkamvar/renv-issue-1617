5cda5ff5f2d59aa6ca93f9ce836fd7a5bcf61ace
Zhian N. Kamvar <zkamvar@gmail.com>
2023-07-31 12:23:23 -0700

    modify script and run to show differences inside and outside the lesson context

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-RENV_STARTUP_DIAGNOSTICS=TRUE R -e 'sandpaper::build_lesson("lsn", preview = FALSE)' 2>&1 | tee out.txt
+CWD=$(pwd)
+RENV_STARTUP_DIAGNOSTICS=TRUE R -e 'tick <- Sys.time(); sandpaper::validate_lesson("lsn"); Sys.time() - tick' 2>&1 | tee inside_renv.txt
+
+RENV_STARTUP_DIAGNOSTICS=TRUE cd lsn && R -e 'tick <- Sys.time(); sandpaper::validate_lesson("."); Sys.time() - tick' 2>&1 | tee ../outside_renv.txt
+
+cd ${CWD}
