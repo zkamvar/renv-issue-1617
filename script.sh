b86f593efc0d7fc3f43da630509b1f0827610473
Zhian N. Kamvar <zkamvar@gmail.com>
2023-07-31 15:29:54 -0700

    run with watchdog; add build lesson examples

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 CWD=$(pwd)
-RENV_STARTUP_DIAGNOSTICS=TRUE R -e 'tick <- Sys.time(); sandpaper::validate_lesson("lsn"); Sys.time() - tick' 2>&1 | tee inside_renv.txt
 
-RENV_STARTUP_DIAGNOSTICS=TRUE cd lsn && R -e 'tick <- Sys.time(); sandpaper::validate_lesson("."); Sys.time() - tick' 2>&1 | tee ../outside_renv.txt
+RENV_WATCHDOG_DEBUG=TRUE R -e 'tick <- Sys.time(); sandpaper::validate_lesson("lsn"); Sys.time() - tick' 2>&1 | tee inside_renv.txt
+
+cd lsn && RENV_WATCHDOG_DEBUG=TRUE R -e 'tick <- Sys.time(); sandpaper::validate_lesson("."); Sys.time() - tick' 2>&1 | tee ../outside_renv.txt
 
 cd ${CWD}
+
+RENV_WATCHDOG_ENABLED=FALSE R -e 'tick <- Sys.time(); sandpaper::build_lesson("lsn", quiet = FALSE, preview = FALSE); Sys.time() - tick' 2>&1 | tee build_lesson_no_watchdog.txt
+
+RENV_WATCHDOG_DEBUG=TRUE R -e 'tick <- Sys.time(); sandpaper::build_lesson("lsn", quiet = FALSE, preview = FALSE); Sys.time() - tick' 2>&1 | tee build_lesson_watchdog.txt
+
